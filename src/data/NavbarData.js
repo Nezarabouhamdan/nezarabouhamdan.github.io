2f7280004b93a6380eb94b30c24431178f30bceb
nezarabouhamdan <nwa200079@gmail.com>
2023-08-01 22:48:04 +0300

    xz

--- src/data/NavbarData.js
+++ src/data/NavbarData.js
@@ -1,16 +1,13 @@
 export const data = [
   {
-    to: "/portofolio",
     text: "Home",
     id: "home",
   },
   {
-    to: "/portofolio",
     text: "Experience",
     id: "Experience",
   },
   {
-    to: "/portofolio",
     text: "Status",
     id: "Status",
   },
